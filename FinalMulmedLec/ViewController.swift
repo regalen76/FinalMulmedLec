7ea07552059e38a135f1670ea6864cbaccda46f0
regalen76 <94056395+regalen76@users.noreply.github.com>
2023-01-26 21:12:24 +0700

    Initial Commit

---
+++ FinalMulmedLec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalMulmedLec
+//
+//  Created by Reonaldo Galen Maliode on 26/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
